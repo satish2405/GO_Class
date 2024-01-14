3f34baaa9c424f16c2a122e7074113b8d7c4c8e2
satish2405 <hclrsatish@gmail.com>
2024-01-14 08:51:17 +0530

    loops concepts basics

--- 08Loop/main.go
+++ 08Loop/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("..loops..")
+
+// 	for i := 0; i <= 100; i++ {
+// 		if i%5 == 0 {
+// 			fmt.Println(i)
+// 		}
+
+// 	}
+// }
+
+// syntax same of while?
 func main() {
 	fmt.Println("..loops..")
-
-	for i := 0; i <= 100; i++ {
+	i := 1
+	for i <= 100 { // remove condition for infinite
 		if i%5 == 0 {
 			fmt.Println(i)
+			break
 		}
-
+		i++ // no increment
 	}
 }
